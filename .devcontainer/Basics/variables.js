6a2764a7f9ea365db851a0851b4e12fe56429346
Rohit Pol <70880354+Rohit0911@users.noreply.github.com>
2024-02-19 20:39:13 +0000

    variables

---
+++ .devcontainer/Basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=14789
+let accountEmail="rohitpol@google.com"
+var accountPassword="1252"
+accountCity="Pune"
+
+
+// accountId=99000 // Const keyword doesn't allow change in the values.
+accountEmail="abc@abc.com"
+accountPassword="79504"
+accountCity="Bengaluru"
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
